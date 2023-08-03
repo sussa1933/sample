0cac8f59079eae74c071685ba041e23004dd274f
sussa1933 <sussa1933@naver.com>
2023-08-03 22:09:31 +0900

    chore: QuestionRepository 인터페잇 & 구현체 삭제

--- src/main/java/com/study/project/question/repository/QuestionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.study.project.question.repository;
-
-import com.study.project.question.entity.Question;
-
-public interface QuestionRepository {
-
-    Question saveQuestion(Question question);
-}

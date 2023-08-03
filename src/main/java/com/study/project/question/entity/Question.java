c82c2b5ee967964650bfa930df9432081c0d6357
sussa1933 <sussa1933@naver.com>
2023-08-03 22:11:05 +0900

    feat: Question Entity 필드 Mapping

--- src/main/java/com/study/project/question/entity/Question.java
+++ src/main/java/com/study/project/question/entity/Question.java
@@ -1,20 +1,39 @@
 package com.study.project.question.entity;
 
 
+import com.study.project.answer.entity.Answer;
+import com.study.project.user.entity.User;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Entity
-@Getter
-@Setter
+@Data
 public class Question {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
-    private String title;
+    @Column(length = 200, nullable = false)
+    private String subject;
+
+    @Column(columnDefinition = "LONGTEXT", nullable = false)
+    private String content;
+
+    @ToString.Exclude
+    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
+    private User author;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("id")
+    private Set<Answer> answerList = new LinkedHashSet<>();
+
+    @ManyToMany
+    private Set<User> voter = new LinkedHashSet<>();
 
 }

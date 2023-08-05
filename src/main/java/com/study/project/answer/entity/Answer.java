68c61ca4465de06f9a83107e65d618e418e8626e
sussa1933 <sussa1933@naver.com>
2023-08-05 22:42:24 +0900

    feat: Answer Entity 구현 및 DB 매핑

--- src/main/java/com/study/project/answer/entity/Answer.java
+++ src/main/java/com/study/project/answer/entity/Answer.java
@@ -1,18 +1,39 @@
 package com.study.project.answer.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import com.study.project.question.entity.Question;
+import com.study.project.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "answers")
 public class Answer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(columnDefinition = "LONGTEXT", nullable = false)
+    private String content;
+
+    @ToString.Exclude
+    @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.LAZY)
+    private User author;
+
+    @ToString.Exclude
+    @ManyToOne(cascade = CascadeType.DETACH, fetch = FetchType.LAZY)
+    private Question question;
+
+    @ManyToMany
+    Set<User> voter = new LinkedHashSet<>();
+
+
 }

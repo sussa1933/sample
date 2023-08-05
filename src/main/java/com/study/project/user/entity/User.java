272afb8c7f0f73701c024aa99d41e1c65e842fa3
sussa1933 <sussa1933@naver.com>
2023-08-05 22:40:52 +0900

    feat: User Entity Create Method 추가 및 연관 관계 편의 Method 추가

--- src/main/java/com/study/project/user/entity/User.java
+++ src/main/java/com/study/project/user/entity/User.java
@@ -3,9 +3,7 @@ package com.study.project.user.entity;
 import com.study.project.answer.entity.Answer;
 import com.study.project.question.entity.Question;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -13,15 +11,23 @@ import java.util.Set;
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(length = 100, nullable = false)
     private String email;
 
+    @Column(length = 100, nullable = false)
     private String username;
 
+    @Column(length = 300, nullable = false)
     private String password;
 
     @ToString.Exclude
@@ -33,5 +39,18 @@ public class User {
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
     @OrderBy("id")
     private Set<Answer> answerList = new LinkedHashSet<>();
+
+    public static User createUser(String email, String username, String password) {
+        User user = User.builder()
+                .email(email)
+                .username(username)
+                .password(password)
+                .build();
+        return user;
+    }
+
+    public void addQuestion(Question question) {
+        questionList.add(question);
+    }
 
 }

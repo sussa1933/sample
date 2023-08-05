ef05068dda17b49071e487b5387db872bf54ef35
sussa1933 <sussa1933@naver.com>
2023-08-05 22:44:48 +0900

    test: User & Question Service 로직 Sample Test 코드 작성

--- src/test/java/com/study/project/ServiceLogicTest.java
+++ src/test/java/com/study/project/ServiceLogicTest.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class SampleTest {
+public class ServiceLogicTest {
 
     @Autowired
     UserService userService;

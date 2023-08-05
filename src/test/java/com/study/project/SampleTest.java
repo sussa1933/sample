4d034cb1e6d782427ef219988a594a4cb5608dc2
sussa1933 <sussa1933@naver.com>
2023-08-05 22:44:14 +0900

    test: User & Question Service 로직 Sample Test 코드 작성

--- src/test/java/com/study/project/SampleTest.java
+++ src/test/java/com/study/project/SampleTest.java
@@ -1,24 +1,62 @@
 package com.study.project;
 
+import com.study.project.question.dto.QuestionRequestDto;
+import com.study.project.question.entity.Question;
+import com.study.project.question.service.QuestionService;
+import com.study.project.user.dto.UserRequestDto;
+import com.study.project.user.entity.User;
+import com.study.project.user.service.UserService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 public class SampleTest {
 
     @Autowired
-    private ApplicationContext ac;
+    UserService userService;
+
+    @Autowired
+    QuestionService questionService;
 
     @Test
-    void 샘플_테스트() {
-        String[] names = ac.getBeanDefinitionNames();
-        for (String name : names) {
-            System.out.println(
-                    "@@@@@ Bean Name = " + name);
-        }
-
+    void createUserTest() {
+        // Given
+        UserRequestDto requestDto = UserRequestDto.builder()
+                .email("test@test.com")
+                .username("testUser")
+                .password1("pw1")
+                .password2("pw1")
+                .build();
+        // When
+        User user = userService.createUser(requestDto);
+        // Then
+        Assertions.assertThat(user.getEmail()).isEqualTo(requestDto.getEmail());
+        Assertions.assertThat(user.getUsername()).isEqualTo(requestDto.getUsername());
     }
 
+    @Test
+    void createQuestionService() {
+        // Given
+        QuestionRequestDto questionRequestDto = QuestionRequestDto.builder()
+                .authorId(1L)
+                .subject("TestSubject")
+                .content("TestContent")
+                .build();
+        UserRequestDto userRequestDto = UserRequestDto.builder()
+                .email("test@test.com")
+                .username("testUser")
+                .password1("pw1")
+                .password2("pw1")
+                .build();
+        // When
+        User user = userService.createUser(userRequestDto);
+        Question question = questionService.createQuestion(questionRequestDto);
+        // Then
+        Assertions.assertThat(user.getEmail()).isEqualTo(userRequestDto.getEmail());
+        Assertions.assertThat(user.getUsername()).isEqualTo(userRequestDto.getUsername());
+        Assertions.assertThat(question.getSubject()).isEqualTo(questionRequestDto.getSubject());
+        Assertions.assertThat(question.getContent()).isEqualTo(questionRequestDto.getContent());
+    }
 }

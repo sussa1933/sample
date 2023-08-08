b087c41ee4e9bd246c7e0049941f724965479b17
sussa1933 <sussa1933@naver.com>
2023-08-08 12:59:37 +0900

    feat: QuestionJpaRepository Keyword 검색 로직 추가

--- src/main/java/com/study/project/question/repository/QuestionJpaRepository.java
+++ src/main/java/com/study/project/question/repository/QuestionJpaRepository.java
@@ -1,11 +1,11 @@
 package com.study.project.question.repository;
 
 import com.study.project.question.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface QuestionJpaRepository extends JpaRepository<Question, Long> {
-
+    Page<Question> findAllBySubjectContainingIgnoreCaseOrContentContainingIgnoreCase(Pageable pageable, String subject, String content);
 
 }

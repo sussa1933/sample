4ec9dd60d13aefcf07b5ac7ee89252dd9ac32934
sussa1933 <sussa1933@naver.com>
2023-08-08 12:58:34 +0900

    feat: QuestionController Auditable Test 위한 임시 API 추가

--- src/main/java/com/study/project/question/controller/QuestionController.java
+++ src/main/java/com/study/project/question/controller/QuestionController.java
@@ -1,7 +1,10 @@
 package com.study.project.question.controller;
 
+import com.study.project.question.dto.QuestionRequestDto;
 import com.study.project.question.entity.Question;
 import com.study.project.question.service.QuestionService;
+import com.study.project.user.dto.UserRequestDto;
+import com.study.project.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class QuestionController {
 
+    private final UserService userService;
 
+    private final QuestionService questionService;
 
+    @GetMapping
+    public void createQuestion() {
+        userService.createUser(UserRequestDto.builder()
+                .email("test@test.com")
+                .username("testUser")
+                .password("pw1")
+                .build());
+        QuestionRequestDto dto = QuestionRequestDto.builder()
+                .authorId(1L)
+                .content("test")
+                .subject("test")
+                .build();
+        questionService.createQuestion(dto);
+    }
 }

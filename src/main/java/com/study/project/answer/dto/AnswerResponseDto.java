f77a58c2775406255b218da7a1dce308461fe7e7
sussa1933 <sussa1933@naver.com>
2023-08-08 12:55:34 +0900

    feat: AnswerResponseDto 작성

---
+++ src/main/java/com/study/project/answer/dto/AnswerResponseDto.java
@@ -0,0 +1,46 @@
+package com.study.project.answer.dto;
+
+import com.study.project.answer.entity.Answer;
+import com.study.project.user.dto.UserResponseDto;
+import com.study.project.user.entity.User;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AnswerResponseDto {
+
+    private Long id;
+
+    private String content;
+
+    private UserResponseDto author;
+
+    List<UserResponseDto> voter;
+
+    private LocalDateTime createDate;
+
+    private LocalDateTime modifyDate;
+
+    public static AnswerResponseDto of(Answer answer) {
+        Set<User> voterSet = answer.getVoter();
+        List<User> voterList = new ArrayList<>(voterSet);
+        List<UserResponseDto> responseDtoList = voterList.stream().map(UserResponseDto::of)
+                .toList();
+        return AnswerResponseDto.builder()
+                .id(answer.getId())
+                .content(answer.getContent())
+                .voter(responseDtoList)
+                .createDate(answer.getCreateDate())
+                .modifyDate(answer.getModifyDate())
+                .build();
+    }
+
+}

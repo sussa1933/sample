5350efa9fb718b0c8f83f402d5d237a3abc25c32
sussa1933 <sussa1933@naver.com>
2023-08-08 12:59:01 +0900

    feat: UserJpaRepository findByUsername 로직 추가

--- src/main/java/com/study/project/user/repository/UserJpaRepository.java
+++ src/main/java/com/study/project/user/repository/UserJpaRepository.java
@@ -3,5 +3,8 @@ package com.study.project.user.repository;
 import com.study.project.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserJpaRepository extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username);
 }

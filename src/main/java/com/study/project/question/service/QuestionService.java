727a61405a9e4bcdda29cf2a988f5eeb387b93ec
sussa1933 <sussa1933@naver.com>
2023-08-08 13:00:06 +0900

    feat: QuestionService 로직 구현

--- src/main/java/com/study/project/question/service/QuestionService.java
+++ src/main/java/com/study/project/question/service/QuestionService.java
@@ -1,14 +1,21 @@
 package com.study.project.question.service;
 
 import com.study.project.question.dto.QuestionRequestDto;
+import com.study.project.question.dto.QuestionResponseDto;
+import com.study.project.question.dto.QuestionSimpleResponseDto;
 import com.study.project.question.entity.Question;
 import com.study.project.question.repository.QuestionJpaRepository;
 import com.study.project.user.entity.User;
 import com.study.project.user.repository.UserJpaRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -20,15 +27,62 @@ public class QuestionService {
     private final UserJpaRepository userJpaRepository;
 
     @Transactional
-    public Question createQuestion(QuestionRequestDto dto) {
+    public QuestionResponseDto createQuestion(QuestionRequestDto dto) {
         String subject = dto.getSubject();
         String content = dto.getContent();
         Long authorId = dto.getAuthorId();
         Optional<User> findUser = userJpaRepository.findById(authorId);
-        User user = findUser.orElseThrow(() -> new RuntimeException("NOT FOUND, Bad Request"));
+        User user = findUser.orElseThrow(() -> new RuntimeException("Question NOT FOUND, Bad Request"));
         Question question = Question.createQuestion(subject, content);
         question.addAuthor(user);
-        return questionJpaRepository.save(question);
+        Question saveQuestion = questionJpaRepository.save(question);
+        return QuestionResponseDto.of(saveQuestion);
+    }
+
+    @Transactional
+    public QuestionResponseDto findQuestion(Long questionId) {
+        Optional<Question> question = questionJpaRepository.findById(questionId);
+        Question findQuestion = question.orElseThrow(() -> new RuntimeException("Question Not Found,Bad Request"));
+        return QuestionResponseDto.of(findQuestion);
+    }
+
+    @Transactional
+    public Page<QuestionSimpleResponseDto> findQuestionList(int page, String kw) {
+        Pageable pageRequest = PageRequest.of(page, 10);
+        Page<Question> findQuestions = questionJpaRepository
+                .findAllBySubjectContainingIgnoreCaseOrContentContainingIgnoreCase(pageRequest, kw, kw);
+        List<QuestionSimpleResponseDto> questionList = findQuestions.getContent()
+                .stream()
+                .map(QuestionSimpleResponseDto::of)
+                .toList();
+        Page<QuestionSimpleResponseDto> response =
+                new PageImpl<>(questionList, pageRequest, findQuestions.getTotalElements());
+        return response;
+    }
+
+    @Transactional
+    public QuestionResponseDto modifyQuestion(Long questionId, QuestionRequestDto dto) {
+        Optional<Question> question = questionJpaRepository.findById(questionId);
+        Question findQuestion = question.orElseThrow(() -> new RuntimeException("Question Not Found, Bad Request"));
+        findQuestion.setContent(dto.getContent());
+        findQuestion.setSubject(dto.getSubject());
+        Question updateQuestion = questionJpaRepository.save(findQuestion);
+        return QuestionResponseDto.of(updateQuestion);
+    }
+
+    @Transactional
+    public void deleteQuestion(Long questionId) {
+        questionJpaRepository.deleteById(questionId);
+    }
+
+    @Transactional
+    public void voteQuestion(Long questionId, Long userId) {
+        Question findQuestion = questionJpaRepository.findById(questionId)
+                .orElseThrow(() -> new RuntimeException("Question Not Found, Bad Request"));
+        User findUser = userJpaRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User Not Found, Bad Request"));
+        findQuestion.getVoter().add(findUser);
+        questionJpaRepository.save(findQuestion);
     }
 
 

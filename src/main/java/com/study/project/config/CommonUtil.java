9a67ea90b455d12615b39f5c279771441b240823
sussa1933 <sussa1933@naver.com>
2023-08-10 17:02:55 +0900

    feat: Common Markdown Util 컴포넌트 작성

---
+++ src/main/java/com/study/project/config/CommonUtil.java
@@ -0,0 +1,18 @@
+package com.study.project.config;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonUtil {
+
+    public String markdown(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node parse = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(parse);
+    }
+
+}

cdefeb677a3b7559994191fe02bde553b3cd59ca
sussa3007 <sussa1933@gmail.com>
2023-08-17 02:03:32 +0900

    chore: Init Data sql 파일 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,25 @@
+insert into `users` (`email`, `username`, `password`)
+values
+    ('test1@test.com', 'aaaa',  '{noop}1111!'),
+    ('test1@test.com', 'bbbb',  '{noop}1111!')
+;
+
+insert into `user_roles`(`user_id`, `roles`)
+values
+    (1, 'USER_ROLE'),
+    (2, 'USER_ROLE')
+;
+
+insert into `questions`(`subject`, `content`, `author_id`)
+values
+    ('test', 'tttatat', 1),
+    ('test1', 'asdgdqdf', 1),
+    ('test2', 'tttasdfasdatat', 1)
+;
+
+insert into `answers`(`content`, `author_id`, `question_id`)
+values
+    ('answer', 2, 1),
+    ('answer111', 2, 1),
+    ('answer', 2, 1)
+;

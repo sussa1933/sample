c21ef1c3b7d9f0184862bbdb484d5edbdcfdcf72
sussa1933@gmail.com <sussa1933@naver.com>
2023-08-17 18:38:00 +0900

    refactor: ErrorCode Cookie 관련 코드 추가

--- src/main/java/com/study/project/exception/ErrorCode.java
+++ src/main/java/com/study/project/exception/ErrorCode.java
@@ -2,25 +2,31 @@ package com.study.project.exception;
 
 import lombok.Getter;
 
+@Getter
 public enum ErrorCode {
 
-    BAD_REQUEST(400, "BAD REQUEST"),
-    USER_EXIST(400, "USER EXIST"),
-    ACCESS_DENIED(403, "ACCESS DENIED"),
-    NOT_FOUND(404, "NOT FOUND"),
-    INTERNAL_SERVER_ERROR(500, "Internal Server Error" ),
-    DATA_ACCESS_ERROR(500, "Data Access Error"),
-    NOT_IMPLEMENTED(501, "NOT IMPLEMENTED");
+    BAD_REQUEST(400, "BAD REQUEST", 14001),
+    USER_EXIST(400, "USER EXIST", 14002),
+    ACCESS_DENIED(403, "ACCESS DENIED", 14003),
+    EXPIRED_ACCESS_TOKEN(403, "Expired Access Token", 14004),
+    NOT_FOUND(404, "NOT FOUND", 14005),
+    NOT_FOUND_COOKIE(404, "NOT FOUND COOKIE", 14006),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error" , 15001),
+    DATA_ACCESS_ERROR(500, "Data Access Error", 15002),
+    NOT_IMPLEMENTED(501, "NOT IMPLEMENTED", 15003);
 
-    @Getter
     private final int status;
 
     @Getter
     private final String message;
 
-    ErrorCode(int code, String message) {
+    @Getter
+    private final int errorCode;
+
+    ErrorCode(int code, String message, int errorCode) {
         this.status = code;
         this.message = message;
+        this.errorCode = errorCode;
     }
 
 }

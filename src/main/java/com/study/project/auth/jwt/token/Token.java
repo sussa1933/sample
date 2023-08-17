67d2a620e2a1d19d645eb605b07878346b79e92b
sussa1933@gmail.com <sussa1933@naver.com>
2023-08-17 18:32:32 +0900

    feat: JWT Tokenizer 서비스 로직 구현 및 Token Dto 객체 작성

---
+++ src/main/java/com/study/project/auth/jwt/token/Token.java
@@ -0,0 +1,18 @@
+package com.study.project.auth.jwt.token;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@NoArgsConstructor
+@Getter
+@AllArgsConstructor
+public class Token {
+
+    private String accessToken;
+
+    private String refreshToken;
+
+}

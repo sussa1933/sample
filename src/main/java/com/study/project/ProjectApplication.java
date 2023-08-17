42ef099ca04ce12cce6b95c2893c3e3eb33666c0
sussa1933@gmail.com <sussa1933@naver.com>
2023-08-17 18:37:37 +0900

    chore: 환경 변수 파일 Init

--- src/main/java/com/study/project/ProjectApplication.java
+++ src/main/java/com/study/project/ProjectApplication.java
@@ -2,8 +2,12 @@ package com.study.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@PropertySource("classpath:/env.yml")
 public class ProjectApplication {
 
 	public static void main(String[] args) {
